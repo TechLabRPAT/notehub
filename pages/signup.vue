bf114ee8bdfb9156e486532c41c13d02f8bf3d92
ARAI-techlab <54737819+ARAI-techlab@users.noreply.github.com>
2019-09-22 18:49:29 +0900

    Add files via upload

---
+++ pages/signup.vue
@@ -0,0 +1,28 @@
+<!--⑤新規登録画面-->
+<template>
+    <div>
+        <b-conatiner fluid>
+            <b-row class="text-center my-3 py-3">
+                <b-col>
+                    <h2>新規会員登録</h2>
+                </b-col>
+            </b-row>
+            <b-row>
+                <b-col class="text-center my-2">
+                    <h5>メールアドレスで登録</h5>    
+                    </b-col>
+            </b-row>
+            <b-row>
+                <b-col class="text-center my-2">
+                    <h5>Facebookで登録</h5>
+                    </b-col>
+            </b-row>
+            <b-row>
+                <b-col class="text-center mt-2 mb-5">
+                    <h5>Googleで登録</h5>
+                    </b-col>
+            </b-row>
+        </b-conatiner>
+
+    </div>
+</template>
