00d4cd8f4baaa2597dd763cfba43ed880456ab22
ARAI-techlab <54737819+ARAI-techlab@users.noreply.github.com>
2019-10-14 17:54:36 +0900

    Add files via upload

---
+++ pages/searchlist.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <b-row class="text-center">
+            <b-col cols="4">
+                <h4>”キーワード”の検索結果</h4>
+            </b-col>
+        </b-row>
+        <b-container fluid class="p-4 bg-muted">
+        <b-row>
+            <b-col>
+            <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+            </b-col>
+            <b-col>
+            <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=58" alt="Image 2"></b-img>
+            </b-col>
+            <b-col>
+            <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=59" alt="Image 3"></b-img>
+            </b-col>
+            <b-col>
+            <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=60" alt="Image 3"></b-img>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+            <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+            </b-col>
+            <b-col>
+            <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=58" alt="Image 2"></b-img>
+            </b-col>
+            <b-col>
+            <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=59" alt="Image 3"></b-img>
+            </b-col>
+            <b-col>
+            <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=60" alt="Image 3"></b-img>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+            <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=54" alt="Image 1"></b-img>
+            </b-col>
+            <b-col>
+            <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=58" alt="Image 2"></b-img>
+            </b-col>
+            <b-col>
+            <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=59" alt="Image 3"></b-img>
+            </b-col>
+            <b-col>
+            <b-img thumbnail fluid src="https://picsum.photos/250/250/?image=60" alt="Image 3"></b-img>
+            </b-col>
+        </b-row>
+        </b-container>
+    </div>
+</template>
