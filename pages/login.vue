c3c0f48e1f097f39e8eca697f4c46f280e017dad
rtakao <takao.ryotaro@gmail.com>
2019-10-14 17:43:09 +0900

    login.vue

---
+++ pages/login.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <b-container fluid>
+            <b-row class="text-center my-3 py-3">
+                <b-col cols="2"></b-col>
+                <b-col cols="8">
+                    <b-img src="~/assets/logo.png" alt="ロゴ画像" fluid></b-img>
+                </b-col>
+                <b-col cols="2"></b-col>
+            </b-row>
+            <b-row class="text-center my-1 py-1">
+                <b-col cols="1"></b-col>
+                <b-col cols="10">
+                    <p>購入に進むためにはログインが必要です。下記からログインをお願い致します。</p>
+                </b-col>
+                <b-col cols="1"></b-col>
+            </b-row>
+            <b-row class="text-center my-1 py-1">
+                <b-col cols="1"></b-col>
+                <b-col cols="10">
+                    <b-button>Facebookでログイン</b-button>
+                </b-col>
+                <b-col cols="1"></b-col>
+            </b-row>
+            <b-row class="text-center my-1 py-1">
+                <b-col cols="1"></b-col>
+                <b-col cols="10">
+                    <b-button>Googleでログイン</b-button>
+                </b-col>
+                <b-col cols="1"></b-col>
+            </b-row>
+            <b-row class="text-center my-4 py-4">
+                <b-col cols="1"></b-col>
+                <b-col cols="10">
+                </b-col>
+                <b-col cols="1"></b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
