7857f43c683f7c519bc8a230cf55417083b4fa09
rtakao <takao.ryotaro@gmail.com>
2019-10-27 15:16:28 +0900

    created purchasednote.vue

---
+++ pages/purchasednote.vue
@@ -0,0 +1,32 @@
+<!--①ホーム画面-->
+<template>
+    <div>
+      <b-container fluid>
+          <b-row class="text-center my-3 py-3">
+              <b-col>
+                  <h1>購入した講義ノート</h1>
+              </b-col>
+          </b-row>
+          <b-row class="text-center muy-3">
+              <b-col cols="4"><img src="" alt="ピックアップコンテンツ1"></b-col>
+              <b-col cols="4"><img src="" alt="ピックアップコンテンツ1"></b-col>
+              <b-col cols="4"><img src="" alt="ピックアップコンテンツ1"></b-col>
+          </b-row>
+          <b-row class="text-center my-3">
+              <b-col cols="4"><img src="" alt="ピックアップコンテンツ1"></b-col>
+              <b-col cols="4"><img src="" alt="ピックアップコンテンツ1"></b-col>
+              <b-col cols="4"><img src="" alt="ピックアップコンテンツ1"></b-col>
+          </b-row>
+      </b-container>
+    </div>
+</template>
+
+<script>
+import IndexMain from '~/components/pages/IndexMain.vue'
+
+export default{
+    components: {
+        IndexMain
+    }
+}
+</script>
