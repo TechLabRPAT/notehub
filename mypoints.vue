12da3e37c29cf70be8420ae688900be2266b23a7
itsuki-kojima <55236692+itsuki-kojima@users.noreply.github.com>
2019-10-31 19:29:26 +0900

    Add files via upload

---
+++ mypoints.vue
@@ -0,0 +1,41 @@
+
+<template>
+    <div>
+        <b-container fluid>
+            <b-row class="text-center">
+                <b-col cols="1"></b-col>
+                <b-col cols="10">
+                    <h1>マイポイント</h1>
+                </b-col>
+                <b-col cols="1"></b-col>
+            </b-row>
+            <b-row class="text-center">
+                <b-col cols="1"></b-col>
+                <b-col cols="10">
+                    <p><b>〇〇</b>pts</p>
+                </b-col>
+                <b-col cols="1"></b-col>
+            </b-row>
+            <b-row class="text-center">
+                <b-col cols="1"></b-col>
+                <b-col cols="10">
+                    <b-button href="⑭(ポイント履歴画面）" class="my-1" colors="black">ポイント履歴</b-button>
+                    <!--⑭ポイント履歴画面に遷移-->
+                    <!--ボタンの背景色ってどうやって変えるか調べる-->
+                    <!--ボタン上の文字って、どうやって左右に寄せるんだ？-->
+                    <!--何らかの情報をここに表示するわけではないので、src属性ではない。あくまで画面遷移はhref。-->
+
+                </b-col>
+                <b-col cols="1"></b-col>
+            </b-row>
+            <b-row class="text-center">
+                <b-col cols="1"></b-col>
+                <b-col cols="10">
+                    <b-button href="⑮ポイントチャージ画面" class="my-1">ポイントチャージ</b-button>
+                    <!--⑮ポイントチャージ画面に遷移-->
+                </b-col>
+                <b-col cols="1"></b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>>
